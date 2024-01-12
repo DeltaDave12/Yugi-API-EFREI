d4aaceb6177be88855cf83276890bcd53ecabc00
David Rossig <david.rossig12@gmail.com>
2024-01-12 09:19:08 +0100

    Avancé app présentation

--- symfony/src/Controller/CartesPersoController.php
+++ symfony/src/Controller/CartesPersoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CartePersoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class CartesPersoController extends AbstractController
 {
     #[Route('/cartes/perso', name: 'app_cartes_perso')]
-    public function index(): Response
+    public function index(CartePersoRepository $cartePersoRepository): Response
     {
+        $cartes = $cartePersoRepository->findAll();
+
         return $this->render('cartes_perso/index.html.twig', [
-            'controller_name' => 'CartesPersoController',
+            'cartes' => $cartes
         ]);
     }
 }
